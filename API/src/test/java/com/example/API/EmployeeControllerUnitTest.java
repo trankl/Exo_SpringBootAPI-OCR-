2586926623a51cfd0721b55826fac168c56dab51
trankl <trankhanhly88@gmail.com>
2021-10-20 12:17:52 +0200

    update for test et reconfigure H2 to connect

---
+++ API/src/test/java/com/example/API/EmployeeControllerUnitTest.java
@@ -0,0 +1,29 @@
+package com.example.API;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.API.controller.EmployeeController;
+import com.example.API.service.EmployeeService;
+
+@WebMvcTest(controllers = EmployeeController.class)
+public class EmployeeControllerUnitTest {
+
+	@Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees"))
+            .andExpect(status().isOk());
+    }
+}
